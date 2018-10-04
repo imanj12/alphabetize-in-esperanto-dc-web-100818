bcfd3a668726e2e591683a51e480b462c6745d56
Iman Jawad <imanj12@gmail.com>
2018-10-04 11:28:43 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,11 @@
 esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  
-  arr.sort_by do |phrase|
+  new_arr = []
+  new_arr = arr.sort_by do |phrase|
     phrase.split("").map do |a|
       esperanto.index(a)
     end
   end
-  
+  new_arr
 end
